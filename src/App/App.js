31918a511dc3b06399b9d5a3f482a77a7f5129ea
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2022-12-02 13:50:14 -0700

    add mock data

--- src/App/App.js
+++ src/App/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Router } from "react-router-dom";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Successfully Deployed <code>A React application</code> to Netlify with CircleCI</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <main className="main">
+        <h1>Hello</h1>
+      </main>
+    </Router>
   );
-}
+};
 
 export default App;
