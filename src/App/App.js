857f4b860204716865089628c152c1a76ee4b3d6
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2022-12-02 13:54:24 -0700

    add complete architecture

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,11 @@
-import { Router } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import "./App.css";
 
 const App = () => {
   return (
-    <Router>
-      <main className="main">
-        <h1>Hello</h1>
-      </main>
-    </Router>
+    <main className="main">
+      <h1>Hello</h1>
+    </main>
   );
 };
 
