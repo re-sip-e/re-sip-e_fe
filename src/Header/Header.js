1d44039aa57dba2e97e03ee2645cf08447d412fc
Leah Young <leah.young21@alumni.colostate.edu>
2022-12-04 14:04:02 -0700

    re-vamp header structure

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,19 +1,43 @@
 /* eslint-disable react/jsx-no-undef */
 import React from "react";
-import { Avatar } from "@chakra-ui/react";
+import { Heading, Avatar } from "@chakra-ui/react";
 import "./Header.css";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
-    <section className="header-view">
-      <h2>Re*sip*e</h2>
-      <div className="nav-btns">
-        <button>Browse</button>
-        <button>Joe's Bar</button>
-        <Avatar name="Joe Schmoe" src="https://bit.ly/code-beast" />
+    <header className="main-header">
+      <div className="header-container">
+        <Heading as="h2" size="2xl">
+          Re*sip*e
+        </Heading>
+        <nav className="main-nav">
+          <ul className="main-nav-list">
+            <li>
+              <NavLink to="/search">Browse</NavLink>
+            </li>
+            <li>
+              <NavLink to="/drinks/1">Joe's Bar</NavLink>
+            </li>
+            <li>
+              <NavLink to="/profile">
+                <Avatar name="Joe Schmoe" src="https://bit.ly/code-beast" />
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </div>
-    </section>
+    </header>
   );
 }
 
 export default Header;
+
+// <section className="header-view">
+//   <Heading as="h2" size="2xl">Re*sip*e</Heading>
+//   <div className="nav-btns">
+//   {/* <IconButton aria-label='Search database' icon={<SearchIcon />} /> */}
+//     <button>Joe's Bar</button>
+//     <Avatar name="Joe Schmoe" src="https://bit.ly/code-beast" />
+//   </div>
+// </section>
