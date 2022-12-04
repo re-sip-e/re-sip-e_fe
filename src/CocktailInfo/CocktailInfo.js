ae5d097cb1ca8ba8e934c6d481deb706537653a1
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2022-12-04 09:27:49 -0700

    add update mock data

--- src/CocktailInfo/CocktailInfo.js
+++ src/CocktailInfo/CocktailInfo.js
@@ -1,7 +1,33 @@
 import { cocktails } from "../mockData";
-
+import {
+  useDisclosure,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalCloseButton,
+  ModalHeader,
+  ModalBody,
+  FormControl,
+  FormLabel,
+  Input,
+  ModalFooter,
+} from "@chakra-ui/react";
+import React from "react";
+import EditCocktail from "../EditCocktail/EditCocktail";
+// import { cocktails } from "../mockData";
 const CocktailInfo = ({ cocktail }) => {
-  return <h1>{cocktail}</h1>;
+  console.log(cocktails);
+  const indCocktail = cocktails.find(
+    (oneCocktail) => oneCocktail.name === cocktail
+  );
+  console.log(indCocktail);
+  return (
+    <div>
+      <h1>{cocktail}</h1>
+      <EditCocktail cocktail={indCocktail} />
+    </div>
+  );
 };
 
 export default CocktailInfo;
---
+++ src/EditCocktail/EditCocktail.js
@@ -0,0 +1,65 @@
+import {
+  useDisclosure,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalCloseButton,
+  ModalHeader,
+  ModalBody,
+  FormControl,
+  FormLabel,
+  Input,
+  ModalFooter,
+} from "@chakra-ui/react";
+import React from "react";
+
+const EditCocktail = ({ cocktail }) => {
+  console.log(cocktail);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const initialRef = React.useRef(null);
+  const finalRef = React.useRef(null);
+
+  return (
+    <>
+      <Button onClick={onOpen}>Make it my own!</Button>
+
+      <Modal
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Edit Cocktail</ModalHeader>
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>Cocktail</FormLabel>
+              <Input ref={initialRef} placeholder={`${cocktail.name}`} />
+            </FormControl>
+
+            <FormControl mr={4}>
+              <FormLabel>Ingredients</FormLabel>
+              <Input placeholder={`${cocktail.ingredients}`} />
+            </FormControl>
+            <FormControl mr={4}>
+              <FormLabel>Steps</FormLabel>
+              <Input placeholder={`${cocktail.steps}`} />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3}>
+              Save
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default EditCocktail;
