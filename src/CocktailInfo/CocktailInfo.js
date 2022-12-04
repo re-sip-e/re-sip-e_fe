9e0371923b147bf99f072b377389921a8f909160
Leah Young <leah.young21@alumni.colostate.edu>
2022-12-04 16:07:54 -0700

    import NavBar onto cocktail pages

--- src/CocktailInfo/CocktailInfo.js
+++ src/CocktailInfo/CocktailInfo.js
@@ -1,7 +1,14 @@
 import { cocktails } from "../mockData";
+import NavBar from "../NavBar/NavBar";
+import { Heading } from "@chakra-ui/react";
 
 const CocktailInfo = ({ cocktail }) => {
-  return <h1>{cocktail}</h1>;
+  return (
+    <div>
+      <NavBar />
+      <Heading as="h1" size="4xl">{cocktail}</Heading>
+    </div>
+  )
 };
 
 export default CocktailInfo;
