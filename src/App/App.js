027fd9f52a9195cedbfe611e58b440d6069a2dd0
Michael Martinelli <turtelboy123@gmail.com>
2022-12-05 09:03:23 -0700

    make it so data shows up on cocktail cards

--- src/App/App.js
+++ src/App/App.js
@@ -33,7 +33,7 @@ const App = () => {
           path="/:cocktail"
           render={({ match }) => (
             <div className="cocktail-info">
-              <CocktailInfo cocktail={match.params.cocktail} />
+              <CocktailInfo cocktail={match.params.cocktail} cocktailData={cocktails.filter((cocktail) => { return cocktail.name === match.params.cocktail })} />
             </div>
           )}
         ></Route>
