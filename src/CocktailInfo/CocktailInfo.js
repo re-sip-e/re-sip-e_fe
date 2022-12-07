5e07fd1a82104a29b1aefe853d8212d842b7f168
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2022-12-06 18:09:43 -0700

    add styling

--- src/CocktailInfo/CocktailInfo.js
+++ src/CocktailInfo/CocktailInfo.js
@@ -1,5 +1,6 @@
 import { cocktails } from "../mockData";
 import NavBar from "../NavBar/NavBar";
+import "./CocktailInfo.css";
 import { Heading, Spinner } from "@chakra-ui/react";
 import { useQuery, gql, useLazyQuery } from "@apollo/client";
 import React, { useEffect, useState } from "react";
@@ -9,11 +10,7 @@ import { from } from "@apollo/client";
 import { RefetchQueriesFunction } from "@apollo/client";
 import { RefetchQueriesResult } from "@apollo/client";
 
-const CocktailInfo = ({ cocktailId, cocktailData }) => {
-  //   const indCocktail = cocktails.find(
-  //     (oneCocktail) => oneCocktail.id === cocktailId
-  //   );
-
+const CocktailInfo = ({ cocktailId }) => {
   const [choosenCocktail, setCocktail] = useState({});
 
   const getOneCocktail = gql`
@@ -30,54 +27,8 @@ const CocktailInfo = ({ cocktailId, cocktailData }) => {
       }
     }
       `;
-  //   const client = new ApolloClient({
-  //     uri: "https://re-sip-e-be.fly.dev/graphql",
-  //     cache: new InMemoryCache(),
-  //   });
-  //   console.log(useQuery(getOneCocktail));
+
   const { loading, error, data } = useQuery(getOneCocktail);
-
-  //   console.log(data);
-  //   console.log(loading);
-  //   console.log(error);
-  //   console.log({ error, data, loading });
-  //   console.log(oneCocktail);
-  //   useQuery(getOneCocktail)
-  //     .then((result) => {
-  //       console.log(result.data);
-  //     })
-  //     .catch((err) => console.log(err.message));
-  //   const fetchData = async () => {};
-  //   const { error, data, loading } = useQuery(getOneCocktail);
-  //   const getData = async () => {
-  //     const { data } = await client.query({
-  //       query: gql`
-  //         query {
-  //           apiDrink(id: ${cocktailId}){
-  //             id
-  //             name
-  //             steps
-  //             imgUrl
-  //             ingredients {
-  //               name
-  //               quantity
-  //             }
-  //           }
-  //         }
-  //           `,
-  //     });
-  //     console.log(data);
-  //     setCocktail(data);
-  //   };
-
-  //   useEffect(() => {
-  //     if (!loading && data.apiDrink?.length > 0) {
-  //       console.log(data, "this is the data");
-  //       setCocktail(data.apiDrink);
-  //     } else {
-  //       console.log("no data yet");
-  //     }
-  //   }, [data]);
 
   const updateCocktail = (id) => {
     const ingredientIndex = choosenCocktail.ingredients.map((ingredient) => {
@@ -94,38 +45,29 @@ const CocktailInfo = ({ cocktailId, cocktailData }) => {
     setCocktail({ choosenCocktail });
   };
 
-  //   return
-  //   const { loading, error, data } = useQuery(getOneCocktail);
-  //   console.log(data);
-  //   console.log(error);
-  //   if (loading) {
-  //     return <Spinner />;
-  //   }
-  //   if (error) {
-  //     return <h1>Error occured</h1>;
-  //   }
-  //   console.log(choosenCocktail);
   console.log(data);
   return loading ? (
     <Spinner />
   ) : (
     <div>
       <NavBar />
-      <Heading as="h1" size="4xl">
-        {/* {cocktail[0].name} */}
-      </Heading>
-      <h2>{`Steps: ${data.apiDrink.steps}`}</h2>
-      <img src={data.apiDrink.imgUrl} />
-      <h3>
-        {data.apiDrink.ingredients.map((ingredient) => {
-          return `  ${ingredient.quantity} of ${ingredient.name} `;
-        })}
-      </h3>
-      <EditCocktail
-        choosenCocktail={data.apiDrink}
-        updateCocktail={updateCocktail}
-        updateSteps={updateSteps}
-      />
+      <div className="cocktail-details">
+        <Heading as="h1" size="4xl">
+          {data.apiDrink.name}
+        </Heading>
+        <h2>{`Steps: ${data.apiDrink.steps}`}</h2>
+        <img src={data.apiDrink.imgUrl} />
+        <h3>
+          {data.apiDrink.ingredients.map((ingredient) => {
+            return `  ${ingredient.quantity} of ${ingredient.name} `;
+          })}
+        </h3>
+        <EditCocktail
+          choosenCocktail={data.apiDrink}
+          updateCocktail={updateCocktail}
+          updateSteps={updateSteps}
+        />
+      </div>
     </div>
   );
 };
