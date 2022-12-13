6670bd6110fd5850f6f207d593c9b42bdd0d73b4
Leah Young <leah.young21@alumni.colostate.edu>
2022-12-13 12:56:07 -0700

    update navbar as single header, add logo and edit icons

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,34 +1,33 @@
 import React from 'react';
 import "./NavBar.css";
-import { Heading, IconButton, Avatar } from "@chakra-ui/react";
+import { IconButton, Avatar } from "@chakra-ui/react";
 import { SearchIcon } from '@chakra-ui/icons';
 import { Link, NavLink } from "react-router-dom";
-import logo from "../assets/joes-bar.png";
+import barLogo from "../assets/bar-counter.png";
+import siteLogo from "../assets/re-sip-e.png";
 
 const NavBar = () => {
   return (
     <section className='alt-header'>
         <div className='alt-header-box head-2'>
-        <Heading as="h4" size="md">
-            <Link to="/">
-                Re*sip*e
-            </Link>
-        </Heading>
+          <Link to="/">
+                <img src={siteLogo} alt="re-sip-e logo" height="180" width="180" className='site-logo'/>
+          </Link>
         <nav className="second-header head-2">
           <div className="second-header-list head-2">
             <div className="search-tab">
               <NavLink to="/search">
-              <IconButton aria-label='Search database' icon={<SearchIcon boxSize={10} />} />
+              <IconButton aria-label='Search database' icon={<SearchIcon boxSize={10} />} boxSize={14} />
               </NavLink>
             </div>
             <div className="bar-tab">
               <NavLink to="/bar/1" className={"bar-icon-box"}>
-                <img src={logo} alt="joes-bar-logo" width={"30px"}/>
+                <img src={barLogo} alt="joes-bar-logo" width={"45rem"}/>
               </NavLink>
             </div>
             <div className="header-avatar">
               <NavLink to="/profile">
-                <Avatar name="Joe Schmoe" src="https://bit.ly/code-beast" />
+                <Avatar name="Joe Schmoe" src="https://bit.ly/ryan-florence" size={"lg"} />
               </NavLink>
             </div>
           </div>
