cc57c95f34b6504d5afda913e0609a159975989b
Leah Young <leah.young21@alumni.colostate.edu>
2022-12-14 13:33:00 -0700

    edit profile page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,83 +1,86 @@
 import React from "react";
-import NavBar from '../NavBar/NavBar'
-import './Profile.css'
-import { Avatar, Heading } from "@chakra-ui/react";
+import NavBar from "../NavBar/NavBar";
+import "./Profile.css";
+import { Avatar, Heading, Button } from "@chakra-ui/react";
 import { gql, useQuery } from "@apollo/client";
 import { Link } from "react-router-dom";
 
-
 const GET_SINGLE_USER = gql`
-query {
+  query {
     user(id: 1) {
       id
       name
       barCount
-      bars{
+      bars {
         id
         name
         drinkCount
       }
     }
-  }`;
+  }
+`;
 
 const Profile = () => {
-    const { loading, error, data } = useQuery(GET_SINGLE_USER)
+  const { loading, error, data } = useQuery(GET_SINGLE_USER);
 
-    if (loading) {
-        return <div>Finding the user...</div>
-    }
-    if (error) {
-        return <div>No User Found</div>
-    }
+  if (loading) {
+    return <div>Loading your info...</div>;
+  }
+  if (error) {
+    return <div>No User Found</div>;
+  }
 
-    const userBarCount = data.user.bars.map(drink => drink.drinkCount)
+  const barDrinkCount = data.user.bars.map((drink) => drink.drinkCount);
 
-    return (
-        <section className="profile-page">
-            <NavBar />
-            <article className="profile-info">
-                <div className="welcome-user">
-            <Avatar name="Joe Schmoe" src="https://bit.ly/ryan-florence" size={"xl"} />
-                <Heading as="h1" size="4xl" className="welcome-user-msg">
-                    Welcome back, {data.user.name}!
-                </Heading>
-
-                </div>
-                <div className="user-bar-data">
-                    <div className="user-bar-count">
-                        <Heading as="h3" size="2xl" className="num-of-bars">Bars</Heading>
-                        {data.user.barCount}
-                    </div>
-                    <div className="bar-drink-count">
-                        <Heading as="h3" size="2xl" className="num-of-drinks">Number of Drinks</Heading>
-                        {userBarCount}
-                    </div>
-                    <Link to="/bar/1">
-                        <button className="view-bar-btn">View my bar</button>
-                    </Link>
-                </div>
-                <div className="user-data">
-                    <div className="personal-info">
-                        <p>Name: {data.user.name}</p>
-                        <p>Email: JSchmoe23@gmail.com</p>
-                        <p>Location: Denver, CO</p>
-                    </div>
-                </div>
-            </article>
-        </section>
-    )
-}
-
+  return (
+    <section className="profile-page">
+      <NavBar />
+      <article className="profile-info">
+        <div className="welcome-user">
+          <Avatar
+            name="Joe Schmoe"
+            src="https://bit.ly/ryan-florence"
+            size={"2xl"}
+          />
+          <Heading as="h2" size="3xl" className="welcome-user-msg">
+            Welcome back, {data.user.name}!
+          </Heading>
+        </div>
+        <div className="user-data-container">
+          <div className="user-bar-data">
+                <Heading as="h3" size="2xl" className="my-bar-info">Bar Status</Heading>
+            <div className="user-bar-count">
+              <Heading as="h4" size="md" className="num-of-bars">
+                <u>Bars</u>
+                <br />
+                <br/>
+              {data.user.barCount}
+              </Heading>
+            </div>
+            <div className="bar-drink-count">
+              <Heading as="h4" size="md" className="num-of-drinks">
+                <u>Number of Drinks</u>
+                <br />
+                <br/>
+              {barDrinkCount}
+              </Heading>
+            </div>
+            <Link to="/bar/1">
+              <Button colorScheme="teal" className="view-bar-btn">View my bar</Button>
+            </Link>
+          </div>
+          <div className="personal-info">
+            <div className="my-info-container">
+          <Heading as="h3" size="2xl" className="my-info">My Info</Heading>
+            </div>
+            <p><b>Name:</b> {data.user.name}</p>
+            <p><b>Email:</b> JSchmoe23@gmail.com</p>
+            <p><b>Location:</b> Denver, CO</p>
+          </div>
+        </div>
+      </article>
+    </section>
+  );
+};
 
 export default Profile;
-
-
-// export const useUserData = (id) => {
-
-//     const { loading, error, data } = useQuery(GET_SINGLE_USER, {
-//         variables: {
-//             id,
-//         }
-//     })
-//     return { data, error, loading };
-// };
